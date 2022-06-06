d8a55264b9638362b2d82e1b5d7b87bbe88438bf
jaguar6105 <gilljoseph603@gmail.com>
2022-06-05 20:22:53 -0400

    add table

--- river_seed.sql
+++ river_seed.sql
@@ -1,4 +1,10 @@
 USE riverAlertDB;
 
-INSERT INTO riverStation (id, river, stationLocation)
+CREATE TABLE riverStation (
+    stationId varchar(30),
+    river varchar(50),
+    stationLocation varchar(50)
+);
+
+INSERT INTO riverStation (stationId, river, stationLocation)
 VALUES ("02037500", "James River", "Richmond");
