1e90a7a2b0eff01465ea9e22458c63974adcd91f
jaguar6105 <gilljoseph603@gmail.com>
2022-06-12 20:48:54 -0400

    Add drop down

--- public/asset/js/home.js
+++ public/asset/js/home.js
@@ -2,27 +2,27 @@
 
 $(document).ready(function () {
 
-//get favs
+  //get favs
 
-// Gets river data from api and renders it
-const getRivers = () => {
+  // Gets river data from api and renders it
+  const getRivers = () => {
     $.get("/db/river", renderData);
   };
 
   //will render individual stations
-const renderData = (data) => {
+  const renderData = (data) => {
     console.log(data);
-};
+  };
 
 
 
-//search rivers
-const searchRivers = () => {
-  let name = document.getElementById('rivername').innerHTML;
-  let url = "/db/river/"+ name;
+  //search rivers
+  const searchRivers = () => {
+    let name = document.getElementById('rivername').innerHTML;
+    let url = "/db/river/" + name;
 
-  $.get(url, renderData);
-};
+    $.get(url, renderData);
+  };
 
   //search button
   document.getElementById('search-button').addEventListener('click', function (event) {
@@ -30,3 +30,35 @@ const searchRivers = () => {
     searchRivers();
   });
 });
+
+const renderRiversData = (data) => {
+
+}
+
+// Gets river data from api and renders it
+const getRiverData = (location) => {
+
+  let client = "Dh7RPSpIbn5Nt8vCSnk5K";
+  let secret = "34hU5GqA4MmKEOzMynoYvL6qL2uFUNKVgQVadnFJ";
+
+
+  let url = "https://api.aerisapi.com/rivers/" + location + "?client_id=" + client + "&client_secret=" + secret;
+  console.log(url);
+
+  $.ajax({
+    url: url
+  })
+    .done(function (json) {
+      if (json.success == true) {
+        let ob = json.response.ob;
+        if (!ob) {
+          ob = "The data is unavailable. Try again later.";
+        }
+        else {
+          //          console.log(ob);
+          renderRiversData(ob);
+        }
+      }
+    });
+}
+
