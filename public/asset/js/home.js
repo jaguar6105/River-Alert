049573840edf2f268e3635e90453f0cffaccfb4e
jaguar6105 <gilljoseph603@gmail.com>
2022-06-15 16:32:04 -0400

    page functions together

--- public/asset/js/home.js
+++ public/asset/js/home.js
@@ -17,12 +17,12 @@ $(document).ready(function () {
 
 
 
-const renderRiversData = (text, state) => {
-  let search = text+","+state;
+const renderRiversData = (input) => {
+  let search = input.id;
   let card = '<div class="card" id="card" style="width: 18rem;">'
   let body = '<div class="card-body" id="card-body">';
   let button = ' <a href="/river/'+search+'" id="clickriver" data-river='+ search + 'class="btn btn-primary">';
-  let title = ' <h5 class="card-title">'+text + ', '+state+'</h5>'
+  let title = ' <h5 class="card-title">'+search+'</h5>'
 //  button.text() = text+ ", " + state;
   $("#holder").append(card);
   $("#card").append(body);
@@ -32,31 +32,34 @@ const renderRiversData = (text, state) => {
 }
 
 // Gets river data from api and renders it
-const getRiverData = (location) => {
+const getRiverData =  (text, state) => {
+  let search = text+","+state;
 
   let client = "Dh7RPSpIbn5Nt8vCSnk5K";
   let secret = "34hU5GqA4MmKEOzMynoYvL6qL2uFUNKVgQVadnFJ";
 
-
-  let url = "https://api.aerisapi.com/rivers/" + location + "?client_id=" + client + "&client_secret=" + secret;
-  console.log(url);
+  
+  let url = "https://api.aerisapi.com/rivers/" + search + "?format=json&client_id=" + client + "&client_secret="+ secret;
+    console.log(url);
 
   $.ajax({
     url: url
-  })
-    .done(function (json) {
-      if (json.success == true) {
-        let ob = json.response.ob;
-        if (!ob) {
-          ob = "The data is unavailable. Try again later.";
-        }
-        else {
-          //          console.log(ob);
+ })
+ .done(function(json) {
+       if (json.success == true) {
+          let ob = json.response[0];
+          if(!ob) {
+            ob =  "The data is unavailable. Try again later."; 
+          }
+//          console.log(ob);
           renderRiversData(ob);
-        }
-      }
-    });
-}
+       }
+       else {
+        let ob =  "The data is unavailable. Try again later."; 
+        renderRiversData(ob);
+       }
+   });
+  }
 
 $("#state").change(function() {
  id = $(this).children(":selected").attr("value");
@@ -65,6 +68,6 @@ $("#state").change(function() {
 $( "#search-button" ).click(function() {
   $("#holder").empty();
   let text = document.getElementById("rivername").value;;
-  renderRiversData(text, id);
+  getRiverData(text, id);
 });
 });
